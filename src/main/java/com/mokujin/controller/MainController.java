c8e4554bb1cd706d2a3f2e7e6f91778efe2640b3
Vania <boringnameishere@gmail.com>
2017-03-19 18:57:34 +0200

    split main controller into itself and profileController

--- src/main/java/com/mokujin/controller/MainController.java
+++ src/main/java/com/mokujin/controller/MainController.java
@@ -90,42 +90,6 @@ public class MainController {
         return "redirect:/profile";
     }
 
-    @GetMapping("/profile")
-    public String profile(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Profile profile = profileService.findByUsername(authentication.getName());
-        model.addAttribute("profile", profile);
-        model.addAttribute("friends", profile.getFriends());
-        model.addAttribute("insideFriendshipRequests", profile.getInsideFriendShipRequests());
-        model.addAttribute("outsideFriendShipRequests", profile.getOutsideFriendShipRequests());
-        return "profile";
-    }
-
-
-    @GetMapping("/search")
-    public String search(@RequestParam("find") String searchRequest, Model model) {
-        Set<Profile> profiles = new HashSet<>();
-        if (searchRequest.contains(" ")) {
-            model.addAttribute("profiles", profileService.findByFullName(searchRequest));
-            return "search";
-        }
-        Set<Profile> byFirstName = profileService.findByFirstName(searchRequest);
-        Set<Profile> byLastName = profileService.findByLastName(searchRequest);
-        Profile byUsername = profileService.findByUsername(searchRequest);
-
-        if (byUsername != null) {
-            profiles.add(byUsername);
-        }
-        if (!byFirstName.isEmpty()) {
-            byFirstName.forEach(profiles::add);
-        }
-        if (!byLastName.isEmpty()) {
-            byLastName.forEach(profiles::add);
-        }
-        model.addAttribute("profiles", profiles);
-        return "search";
-    }
-
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -133,46 +97,6 @@ public class MainController {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
         return "redirect:/login?logout";
-    }
-
-    @PostMapping("/add")
-    public String addFriend(@RequestParam("username") String friendName) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
-        profileService.sendFriendshipRequest(username, friendName);
-        return "redirect:/profile";
-    }
-
-    @PostMapping("/accept")
-    public String acceptFriendship(@RequestParam("username") String friendName) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
-        profileService.acceptFriendship(username, friendName);
-        return "redirect:/profile";
-    }
-
-    private Profile setNewProfileProperties(Profile profile) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Profile savedProfile = profileService.findByUsername(authentication.getName());
-        if (profile.getUsername() != null) {
-            savedProfile.setUsername(profile.getUsername());
-        }
-        if (profile.getFirstName() != null) {
-            savedProfile.setFirstName(profile.getFirstName());
-        }
-        if (profile.getLastName() != null) {
-            savedProfile.setLastName(profile.getLastName());
-        }
-        if (profile.getEmail() != null) {
-            savedProfile.setEmail(profile.getEmail());
-        }
-        if (profile.getPassword() != null) {
-            savedProfile.setUsername(profile.getPassword());
-        }
-        if (profile.getPhoto() != null) {
-            savedProfile.setPhoto(profile.getPhoto());
-        }
-        return savedProfile;
     }
 
     private byte[] convertMultiPartFileToByteArray(MultipartFile file) {
