381f79151c6b96c66a0143370770bf4feb20b716
Vania <boringnameishere@gmail.com>
2017-06-10 01:46:12 +0300

    added header for pages

--- src/main/java/com/mokujin/config/SecurityConfiguration.java
+++ src/main/java/com/mokujin/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
@@ -26,12 +27,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/img/fullLogo");
+
+        web.ignoring().antMatchers("/img/logo");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/registration").permitAll()
                 .anyRequest().authenticated();
+
 
         http
                 .authorizeRequests()
--- src/main/java/com/mokujin/controller/ImageController.java
+++ src/main/java/com/mokujin/controller/ImageController.java
@@ -33,6 +33,26 @@ public class ImageController {
         response.getOutputStream().close();
     }
 
+    @GetMapping("/logo")
+    public void showLogo(HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("image/jpeg, image/jpg, image/png, image/gif");
+        File defaultImage = new File(System.getProperty("user.dir"),"src/main/resources/public/image/header-logo.jpg");
+        byte[] logo = Files.readAllBytes(defaultImage.toPath());
+        response.getOutputStream().write(logo);
+        response.getOutputStream().close();
+    }
+
+    @GetMapping("/fullLogo")
+    public void showFullLogo(HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("image/jpeg, image/jpg, image/png, image/gif");
+        File defaultImage = new File(System.getProperty("user.dir"),"src/main/resources/public/image/logo.png");
+        byte[] logo = Files.readAllBytes(defaultImage.toPath());
+        response.getOutputStream().write(logo);
+        response.getOutputStream().close();
+    }
+
     private byte[] getDefaultPhoto() throws IOException {
         File defaultImage = new File(System.getProperty("user.dir"),"src/main/resources/public/image/default.jpg");
         return Files.readAllBytes(defaultImage.toPath());
